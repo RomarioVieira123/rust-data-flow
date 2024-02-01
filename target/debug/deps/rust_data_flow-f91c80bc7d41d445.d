d72ca86788550a0b61722cd9726c26320417b131
Romario <romario.vieira@hammerconsult.com.br>
2024-01-31 21:41:12 -0300

    Criação da estrutura do projeto

---
+++ target/debug/deps/rust_data_flow-f91c80bc7d41d445.d
@@ -0,0 +1,5 @@
+F:\Projects\Rust Projects\rust-data-flow\target\debug\deps\rust_data_flow-f91c80bc7d41d445.rmeta: src\main.rs
+
+F:\Projects\Rust Projects\rust-data-flow\target\debug\deps\rust_data_flow-f91c80bc7d41d445.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/rust-data-flow.d
@@ -0,0 +1,1 @@
+F:\Projects\Rust\ Projects\rust-data-flow\target\debug\rust-data-flow.exe: F:\Projects\Rust\ Projects\rust-data-flow\src\main.rs
