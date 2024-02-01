2c0038423696b29882748da0cee13b830f9b8a2c
RomarioVieira123 <romario.lara.vieira@gmail.com>
2024-01-31 21:42:07 -0300

    Delete past target

--- target/debug/deps/rust_data_flow.d
+++
@@ -1,5 +0,0 @@
-F:\Projects\Rust Projects\rust-data-flow\target\debug\deps\rust_data_flow.exe: src\main.rs
-
-F:\Projects\Rust Projects\rust-data-flow\target\debug\deps\rust_data_flow.d: src\main.rs
-
-src\main.rs:
--- target/debug/rust-data-flow.d
+++
@@ -1,1 +0,0 @@
-F:\Projects\Rust\ Projects\rust-data-flow\target\debug\rust-data-flow.exe: F:\Projects\Rust\ Projects\rust-data-flow\src\main.rs
